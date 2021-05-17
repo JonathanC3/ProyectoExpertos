987dd90c38c2b5140bc228eed81b93099f4a023c
JonathanC3 <jonathanc309@gmail.com>
2021-05-16 19:40:02 -0600

    adición del mapa

--- publico/footer.php
+++ publico/footer.php
@@ -3,9 +3,6 @@
         <div class="col-md-12" style="text-align: center;color: black;">
             <h3 class="text-uppercase">Informatica Empresarial</h3>
             <h4 class="text-uppercase">Sistemas Expertos</h4>
-            <p><i class="fas fa-chalkboard-teacher"></i>&nbsp;&nbsp;Jonathan Castro Ulloa y Pablo Castillo</p>
-            <p><span class="glyphicon glyphicon-envelope"></span>&nbsp;&nbsp;jonathan.castro@ucr.ac.cr</p>
-            <p><span class="glyphicon glyphicon-envelope"></span>&nbsp;&nbsp;pablo.castilloquesada@ucr.ac.cr</p>
             <p><span class="glyphicon glyphicon-envelope"></span>&nbsp;&nbsp;2021</p>
         </div>
     </div>

65c7f7229e30119e4cf03504849e9b5867cc1c51
JonathanC3 <jonathanc309@gmail.com>
2021-05-19 00:11:29 -0600

    adición de vistas necesarias para el primer avance

---
+++ vista/vistaCreditos.php
@@ -0,0 +1,42 @@
+<?php
+    include_once 'publico/header.php';
+    
+?>
+<div class="about" id="about">
+    <center>
+        <div class="about-info">
+            <h1>Créditos</h1>
+        </div>
+        <div class="about-top-grids">
+            <div class="col-md-12 about-top-grid">
+                <h4>Acerca de:</h4>
+                <h5>El presente proyecto tiene como propósito es recomendar destinos turísticos cercanos a un lugar, tomando en cuenta restricciones de tiempo, tipo de camino, precios y otras características..</h5>
+            </div>
+            <div class="clearfix"> </div>
+        </div>
+        <br>
+        <div class="fact">
+            <div class="fact-line">
+                <div class="line-1">
+                </div>
+                <h4>Integrantes</h4>
+                <div class="line-2">
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+            <br>
+            <center>
+                <p>Jonathan Castro Ulloa.</p>
+                <p>Pablo Castillo Quesada.</p>
+                <p>Infomática Empresarial</p>
+                <p>2021</p>
+            </center>
+        </div>
+    </center>        
+</div>
+
+    
+<?php
+    include_once 'publico/footer.php';
+?>
+
---
+++ vista/vistaMapa.php
@@ -0,0 +1,15 @@
+<?php
+    include_once 'publico/header.php';
+    
+?>
+    <div class="container"  style="margin-top:30px">
+        <h1>Prototipo de la página del mapa</h1>
+        <center>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d251541.462007909!2d-83.82186412298469!3d9.906003124536747!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0d69b084d750d%3A0x7cb6a6e23ed66ac9!2sProvincia%20de%20Cartago%2C%20Turrialba!5e0!3m2!1ses-419!2scr!4v1621215024570!5m2!1ses-419!2scr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </center>  
+    </div>
+
+<?php
+    include_once 'publico/footer.php';
+?>
+
